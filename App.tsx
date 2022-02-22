dd51f8b81db8e68f4c88f301a09ed5b889202ab0
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2022-02-22 10:53:43 +0100

    data json

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
-
+import { TermsScreen } from './src/screens/TermsScreen';
+import { StarshipFeedScreen } from './src/screens/StarshipFeedScren';
 const App = () => {
-  return <LoginScreen />;
+  // <LoginScreen />;
+  // <TermsScreen />;
+  <StarshipFeedScreen />;
 };
 
 export default App;
